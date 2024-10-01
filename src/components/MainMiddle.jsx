88e2716d1f64edde92df4cbe778b36caa04ecbd6
orzcode <71000311+orzcode@users.noreply.github.com>
2024-10-01 17:54:34 +1300

    background switcher

--- src/components/MainMiddle.jsx
+++ src/components/MainMiddle.jsx
@@ -1,30 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TenCards from "./TenCards.jsx";
 import Splash from "./Splash.jsx";
 
 function MainMiddle() {
   const [mode, setMode] = useState(100);
   const [view, setView] = useState("Splash");
+  const [backgroundImage, setBackgroundImage] = useState("");
 
   const renderView = () => {
     switch (view) {
       case "Splash":
-        return <Splash setView={setView} setMode={setMode}/>;
+        return <Splash setView={setView} setMode={setMode} />;
       case "TenCards":
-        return <TenCards mode={mode} setView={setView}/>;
+        return <TenCards mode={mode} setView={setView} />;
       //remember: clear tencards when mode changes - a 'later' problem
+
       case "AnotherView":
-        return <AnotherView setView={setView}/>;
+        return <AnotherView setView={setView} />;
       default:
         return <p>View not found</p>;
     }
   };
 
+  //Background switcher
+  //could randomize if wanted
+  useEffect(() => {
+    switch (view) {
+      case "Splash":
+        setBackgroundImage(
+          `url('./src/assets/karate.png'), url('./src/assets/kabuki.png')`
+        );
+        break;
+      case "TenCards":
+        setBackgroundImage(
+          `url('./src/assets/garden.png'), url('./src/assets/monk.png')`
+        );
+        break;
+      case "AnotherView":
+        setBackgroundImage(
+          `url('./src/assets/karate.png'), url('./src/assets/kabuki.png')`
+        );
+        break;
+    }
+  }, [view]);
+
   return (
-    <main className="MainMiddle">
-
+    <main className="MainMiddle" style={{ backgroundImage }}>
       {renderView()}
-
     </main>
   );
 }
